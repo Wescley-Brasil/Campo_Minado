b9e98b9fde123ff6390c194f4aa74d7ee6c72ca8
Wescley Brasil <wescley_brasil@hotmail.com>
2018-04-01 21:57:56 -0300

    Persistencia dos dados e correçoes nas funcoes

--- Campo_Minado/Program.cs
+++ Campo_Minado/Program.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
-namespace Campo_Minado {
+namespace Campo_Minado
+{
 
-    class Tabuleiro {
+    class Tabuleiro
+    {
 
         //Minas para controlar a logica e Tabuleiro para servir como interface
         private char[,] tabuleiro = new char[5, 5];
@@ -18,9 +21,12 @@ namespace Campo_Minado {
 
 
         //Metodo para preencher tabuleiro de minas com zeros;
-        public void IniciarMinas() {
-            for (int linha = 1; linha < minas.GetLength(1); linha++) {
-                for (int coluna = 1; coluna < minas.GetLength(1); coluna++) {
+        public void IniciarMinas()
+        {
+            for (int linha = 0; linha < minas.GetLength(1); linha++)
+            {
+                for (int coluna = 0; coluna < minas.GetLength(1); coluna++)
+                {
                     minas[linha, coluna] = 0;
                 }
             }
@@ -28,125 +34,136 @@ namespace Campo_Minado {
         }
 
         //Sortear minhas entre as posicoes 1 e 9, adicionando o valor -1 para as minas nas posicoes vazias
-        public void SortearMinas() {
+        public void SortearMinas()
+        {
             bool sorteado;
             int linha, coluna;
             Random gerarNumero = new Random();
 
-            for (int i = 1; i <= 6; i++) {
-                do {
+            for (int i = 1; i <= 5; i++)
+            {
+                do
+                {
 
                     linha = gerarNumero.Next(3) + 1;
                     coluna = gerarNumero.Next(3) + 1;
 
-                    if (minas[linha, coluna] == -1)
+                    if (minas[linha, coluna] == 1)
                         sorteado = true;
                     else
                         sorteado = false;
 
                 } while (sorteado);
 
-                minas[linha, coluna] = -1;
+                minas[linha, coluna] = 1;
                 contadorFimJogoMinas++;
             }
 
         }
 
-        public void IniciarTabuleiro() {
+        public void IniciarTabuleiro()
+        {
 
-            for (int linha = 1; linha < tabuleiro.GetLength(1) - 1; linha++) {
-                for (int coluna = 1; coluna < tabuleiro.GetLength(1) - 1; coluna++) {
+            for (int linha = 1; linha < tabuleiro.GetLength(1) - 1; linha++)
+            {
+                for (int coluna = 0; coluna < tabuleiro.GetLength(1); coluna++)
+                {
                     tabuleiro[linha, coluna] = '-';
                     contadorFimJogoTabuleiro++;
                 }
             }
         }
 
-        public bool Jogada() {
-
-            //Incrementar rodadas
+        public bool Jogada(int coluna, int linha, char acao)
+        {
             bool resultado = false;
+            //Incrementar rodadas
 
-            if (Ganhou()) {
-                resultado = true;
+            rodadas++;
+
+            Console.Clear();
+
+            #region if
+            if (tabuleiro[linha, coluna] == '?' & acao == 'm')
+            {
+                tabuleiro[linha, coluna] = '-';
+
+                //Verificador fim de jogo
+                if (minas[linha, coluna] == 1)
+                {
+                    contadorFimJogoMinas++;
+                }
             }
+            else if (tabuleiro[linha, coluna] == '-' & acao == 'm')
+            {
+                tabuleiro[linha, coluna] = '?';
 
-            else {
-                rodadas++;
-
-
-
-                int coluna;
-                int linha;
-                char acao;
-                Console.WriteLine("\nDigite linha e coluna entre 1 e 9 e Ação de 'a' para ABRIR e 'm' para MARCAR");
-
-
-                Console.Write("\nDigite o numero da coluna: ");
-                coluna = int.Parse(Console.ReadLine());
-                Console.Write("Digite o numero da linha: ");
-                linha = int.Parse(Console.ReadLine());
-                Console.Write("Digite uma acão: ");
-                acao = Console.ReadLine().ToCharArray()[0];
-
-
-                Console.Clear();
-
-
-                #region if
-                if (tabuleiro[linha, coluna] == '?' & acao == 'm') {
-                    tabuleiro[linha, coluna] = '-';
-
-                    //Verificador fim de jogo
-                    if (minas[linha, coluna] == -1) {
-                        contadorFimJogoMinas++;
-                    }
-                }
-                else if (tabuleiro[linha, coluna] == '-' & acao == 'm') {
-                    tabuleiro[linha, coluna] = '?';
-
-                    //Verificador fim de jogo
-                    if (minas[linha, coluna] == -1) {
-                        contadorFimJogoMinas--;
-                    }
-                }
-                else if (tabuleiro[linha, coluna] == '?' & acao != 'm') {
-
-                }
-                else if (minas[linha, coluna] == -1) {
-                    //resultado = true;
-                    tabuleiro[linha, coluna] = '?';
-                    //GameOver(linha, coluna);
+                //Verificador fim de jogo
+                if (minas[linha, coluna] == 1)
+                {
                     contadorFimJogoMinas--;
-
                 }
-                else if (tabuleiro[linha, coluna] == ' ') {
-
-                }
-                else if (intervalos.Contains(tabuleiro[linha, coluna])) {
-
-                }
-                else {
-                    Revela(linha, coluna);
-                }
-                #endregion
             }
+            else if (tabuleiro[linha, coluna] == '?' & acao != 'm')
+            {
 
+            }
+            else if (minas[linha, coluna] == 1)
+            {
+                resultado = true;
+                GameOver(linha, coluna);
+                contadorFimJogoMinas--;
+
+            }
+            else if (tabuleiro[linha, coluna] == ' ')
+            {
+
+            }
+            else if (intervalos.Contains(tabuleiro[linha, coluna]))
+            {
+
+            }
+            else
+            {
+                Revela(linha, coluna);
+            }
+            #endregion
+
+            //Gravar os dados apos a jogada
+            GravarDados(linha, coluna, acao);
             return resultado;
+        }
 
+        public void zerarJogo()
+        {
+            //Resetar variaveis para iniciar novo jogo
+            string ficheiroMina = @"mina.txt";
+            File.Delete(ficheiroMina);
+
+            string ficheiroMenu = @"menu.txt";
+            File.Delete(ficheiroMenu);
+
+            string ficheiroTabuleiro = @"tabuleiro.txt";
+            File.Delete(ficheiroTabuleiro);
+
+            rodadas = 0;
+            contadorFimJogoMinas = 0;
+            contadorFimJogoTabuleiro = 0;
         }
 
 
-
-
-        // preenche espaços com numeros de bombas ao redor, espaços vazios
-        public void Revela(int linha, int coluna) {
+        
+        public void Revela(int linha, int coluna)
+        {
+            // preenche espaços com numeros de bombas ao redor, espaços vazios
             int contador = 0;
 
             #region for
-            for (int i = -1; i < 2; i++) {
-                for (int j = -1; j < 2; j++) {
-                    if (minas[linha + i, coluna + j] == -1)
+            for (int i = -1; i < 2; i++)
+            {
+                for (int j = -1; j < 2; j++)
+                {
+                    if (minas[linha + i, coluna + j] == 1)
                         contador++;
                 }
 
@@ -155,13 +172,18 @@ namespace Campo_Minado {
             #endregion
 
             #region if
-            if (contador != 0) {
+            if (contador != 0)
+            {
                 tabuleiro[linha, coluna] = Convert.ToString(contador).ToCharArray()[0];
             }
-            else {
-                for (int i = -1; i < 2; i++) {
-                    for (int j = -1; j < 2; j++) {
-                        if (intervalos.Contains(tabuleiro[linha + i, coluna + j])) {
+            else
+            {
+                for (int i = -1; i < 2; i++)
+                {
+                    for (int j = -1; j < 2; j++)
+                    {
+                        if (intervalos.Contains(tabuleiro[linha + i, coluna + j]))
+                        {
                             continue;
                         }
                         tabuleiro[linha + i, coluna + j] = ' ';
@@ -173,22 +195,27 @@ namespace Campo_Minado {
 
         }
 
-        public void TabuleiroInterface() {
+        public void TabuleiroInterface()
+        {
             Console.WriteLine("Rodada: " + rodadas);
             Console.Write("\n\nColunas");
 
-            for (int coluna = 1; coluna < tabuleiro.GetLength(1) - 1; coluna++) {
+            for (int coluna = 1; coluna < tabuleiro.GetLength(1) - 1; coluna++)
+            {
                 Console.Write("\t   " + coluna);
             }
 
             Console.Write("\nLinhas");
 
-            for (int linha = 1; linha < tabuleiro.GetLength(1) - 1; linha++) {
+            for (int linha = 1; linha < tabuleiro.GetLength(1) - 1; linha++)
+            {
                 Console.Write("\n " + linha);
-                for (int coluna = 1; coluna < tabuleiro.GetLength(1) - 1; coluna++) {
+                for (int coluna = 1; coluna < tabuleiro.GetLength(1) - 1; coluna++)
+                {
                     Console.Write("\t|  " + tabuleiro[linha, coluna] + "  |");
                     //Identar interface
-                    if (coluna == 9) {
+                    if (coluna == 9)
+                    {
                         Console.WriteLine();
                     }
                 }
@@ -197,45 +224,248 @@ namespace Campo_Minado {
 
         }
 
+        public void RevelaTabuleiro()
+        {
+            Console.WriteLine("Rodada: " + rodadas);
+            Console.Write("\n\nColunas");
+
+            for (int coluna = 1; coluna < tabuleiro.GetLength(1) - 1; coluna++)
+            {
+                Console.Write("\t   " + coluna);
+            }
+
+            Console.Write("\nLinhas");
+
+            for (int linha = 1; linha < tabuleiro.GetLength(1) - 1; linha++)
+            {
+                Console.Write("\n " + linha);
+                for (int coluna = 1; coluna < tabuleiro.GetLength(1) - 1; coluna++)
+                {
+                    if (minas[linha, coluna] == 1 && tabuleiro[linha, coluna] == '-')
+                    {
+                        Console.Write("\t|  " + 'b' + "  |");
+                    }
+                    else
+                    {
+                        Console.Write("\t|  " + tabuleiro[linha, coluna] + "  |");
+                    }
+                    //Identar interface
+                    if (coluna == 9)
+                    {
+                        Console.WriteLine();
+                    }
+                }
+            }
+        }
 
 
-        public void IniciarJogo() {
+        public void GravarDados(int linha, int coluna, char acao)
+        {
+            string ficheiroMenu = @"menu.txt";
+            string ficheiroTabuleiro = @"tabuleiro.txt";
+            string ficheiroMina = @"mina.txt";
 
+            StreamWriter swMenu;
+            StreamWriter swTabuleiro;
+            StreamWriter swMinas;
+
+
+            swMenu = File.CreateText(ficheiroMenu);
+            swTabuleiro = File.CreateText(ficheiroTabuleiro);
+            swMinas = File.CreateText(ficheiroMina);
+
+            //Salvar estado menu
+            string linhaMenu = contadorFimJogoMinas.ToString() + ";" + contadorFimJogoTabuleiro.ToString() + ";" + rodadas.ToString();
+            swMenu.WriteLine(linhaMenu);
+            //Salvar estado do campo minado e estado tabuleiro
+
+            for (int linhas = 1; linhas < tabuleiro.GetLength(1) - 1; linhas++)
+            {
+                for (int colunas = 1; colunas < tabuleiro.GetLength(1) - 1; colunas++)
+                {
+
+                    swTabuleiro.Write(tabuleiro[linhas, colunas] + ";");
+                    swMinas.Write(minas[linhas, colunas] + ";");
+                }
+            }
+
+            swMenu.Close();
+            swMinas.Close();
+            swTabuleiro.Close();
+        }
+
+        public bool RecuperarDados()
+        {
+            string ficheiroMenu = @"menu.txt";
+            string ficheiroTabuleiro = @"tabuleiro.txt";
+            string ficheiroMina = @"mina.txt";
+            bool estado = false;
+
+            StreamReader srMenu;
+            StreamReader srTabuleiro;
+            StreamReader srMinas;
+
+
+            if (File.Exists(ficheiroMina))
+            {
+                estado = true;
+                srMenu = File.OpenText(ficheiroMenu);
+                srTabuleiro = File.OpenText(ficheiroTabuleiro);
+                srMinas = File.OpenText(ficheiroMina);
+
+                string[] infTabuleiro = srTabuleiro.ReadLine().Split(';');
+                string[] infMinas = srMinas.ReadLine().Split(';');
+                string[] infMenu = srMenu.ReadLine().Split(';');
+
+                contadorFimJogoMinas = Convert.ToInt32(infMenu[0]);
+                contadorFimJogoTabuleiro = Convert.ToInt32(infMenu[1]);
+                rodadas = Convert.ToInt32(infMenu[2]);
+
+                int contadorLista = 0;
+
+                //Recuperar estado do Tabuleiro e Minas
+                for (int linha = 1; linha < tabuleiro.GetLength(1) - 1; linha++)
+                {
+                    for (int coluna = 1; coluna < tabuleiro.GetLength(1) - 1; coluna++)
+                    {
+                        tabuleiro[linha, coluna] = Convert.ToChar(infTabuleiro[contadorLista]);
+                        minas[linha, coluna] = Convert.ToInt32(infMinas[contadorLista]);
+                        contadorLista++;
+                    }
+                }
+
+                srMinas.Close();
+                srTabuleiro.Close();
+                srMenu.Close();
+            }
+            return estado;
+        }
+
+        public void CarregarJogo()
+        {
+            Console.Clear();
             IniciarMinas();
             SortearMinas();
             IniciarTabuleiro();
+        }
 
+        public void IniciarJogo()
+        {
             bool estadoJogo = false;
 
-            do {
-                TabuleiroInterface();
-                estadoJogo = Jogada();
+            do
+            {
+                if (Ganhou() == true)
+                {
+                    estadoJogo = true;
+                }
+
+                else
+                {
+                    TabuleiroInterface();
+
+                    Console.WriteLine("\nDigite linha e coluna entre 1 e 9 e 'a' para ABRIR e 'm' para MARCAR");
+
+                    Console.Write("\nDigite o numero da coluna: ");
+                    int coluna = int.Parse(Console.ReadLine());
+                    Console.Write("Digite o numero da linha: ");
+                    int linha = int.Parse(Console.ReadLine());
+                    Console.Write("Digite uma acão: ");
+                    char acao = Console.ReadLine().ToCharArray()[0];
+
+                    estadoJogo = Jogada(coluna, linha, acao);
+                }
 
 
             } while (!estadoJogo);
-            if (estadoJogo & Ganhou() == true) {
+            if (Ganhou() == true)
+            {
 
-                Console.Clear();
                 Console.Write("\nParabéns voçe venceu");
+                Console.Write("\n\n\n\nDigite Enter para continuar");
 
             }
             Console.ReadKey();
+            zerarJogo();
+            Console.Clear();
+            
+            
+        }
+
+
+        public void Menu()
+        {
+
+            char opcao = ' ';
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Campo Minado\n");
+                Console.WriteLine("Digite 1 para Iniciar novo jogo");
+                Console.WriteLine("Digite 2 para Continuar");
+                Console.WriteLine("Digite 0 para Sair");
+
+                Console.Write("Opcao: ");
+                opcao = Convert.ToChar(Console.ReadLine());
+
+
+
+                switch (opcao)
+                {
+                    case '1':
+                        CarregarJogo();
+                        IniciarJogo();
+                        break;
+
+                    case '2':
+                        if (RecuperarDados())
+                        {
+                            Console.Clear();
+                            IniciarTabuleiro();
+                            IniciarMinas();
+                            RecuperarDados();
+                            IniciarJogo();
+                        }
+                        else
+                        { 
+                             Console.WriteLine("Não existe jogo salvo");
+                             Console.ReadKey();
+                        }
+                        break;
+
+                    case '0':
+                        break;
+                    default:
+                        Console.WriteLine("Opcao inválida");
+                        Console.ReadKey();
+                        break;
+                }
+            } while (opcao != '0');
 
         }
 
-        public void GameOver(int linha, int coluna) {
-            TabuleiroInterface();
-            Console.WriteLine("Fim de jogo");
+        public void GameOver(int linha, int coluna)
+        {
+            RevelaTabuleiro();
+            Console.WriteLine("\nFim de jogo");
+
+
+
         }
 
-        public bool Ganhou() {
+
+        public bool Ganhou()
+        {
 
             bool venceu = false;
             int contadorTabuleiro = 0;
 
-            for (int linha = 1; linha < tabuleiro.GetLength(1) - 1; linha++) {
-                for (int coluna = 1; coluna < tabuleiro.GetLength(1) - 1; coluna++) {
-                    if (tabuleiro[linha, coluna] == '-') {
+            for (int linha = 1; linha < tabuleiro.GetLength(1) - 1; linha++)
+            {
+                for (int coluna = 1; coluna < tabuleiro.GetLength(1) - 1; coluna++)
+                {
+                    if (tabuleiro[linha, coluna] == '-')
+                    {
                         contadorTabuleiro++;
                     }
                 }
@@ -243,7 +473,8 @@ namespace Campo_Minado {
 
             contadorFimJogoTabuleiro = contadorTabuleiro;
 
-            if (contadorFimJogoTabuleiro < 1 & contadorFimJogoMinas < 1) {
+            if (contadorFimJogoTabuleiro < 1 & contadorFimJogoMinas < 1)
+            {
                 venceu = true;
             }
 
@@ -251,32 +482,7 @@ namespace Campo_Minado {
         }
 
 
-        static void Main(string[] args) {
-
-            //bool verify = true;
-            //if (!verify) {
-            //    Console.WriteLine("Deu certo");
-            //}
-
-
-
-            //Console.Write("Linha: ");
-            //int linha = int.Parse(Console.ReadLine());
-            //Console.Write("Coluna: ");
-            //int Coluna = int.Parse(Console.ReadLine());
-            //Console.Write("Digite uma acão: ");
-            //char acao = char.Parse(Console.ReadLine());
-            //Console.ReadKey();
-
-            Tabuleiro tb = new Tabuleiro();
-            tb.IniciarJogo();
-
-
-
-
-        }
     }
 
 
 }
-
